ac46dc266dbd4adcf2af954cebb9615b29ff5213
Juan Robledo <heaxenz@Rikos-PC>
2023-05-05 17:11:58 -0700

    finished

---
+++ app.js
@@ -0,0 +1,16 @@
+  //ES5 Global Constants
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+  //ES2015 Global Constants
+  let PI = 3.14;
+  PI = 42;
+
+    //Quiz
+    // 1. Var affects the global scope while let only affects the block scope; Var can be redeclared while let cannot;
+
+    //2. Var can be redeclared and reassigned freely. While const can only be assigned and declared once.
+    
+    //3. let can be reassigned while const cannot be reassigned or redeclared;
+
+    //4. Hoisting is the order in which the JS complier works. Variables are hoisted to the tope of the scope they are delcared in.
